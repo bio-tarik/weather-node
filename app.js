b3bceeb56ba4668c369c18648ce01518c93fb76f
bio-tarik <bio.tarik@gmail.com>
2018-06-11 19:08:24 -0300

    Moving the request logic to another file

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=rua%20vergueiro%20500%20sao%20paulo',
-    json: true
-}, (error, response, body) => {
-    console.log('body', JSON.stringify(body, undefined, 4));
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(result, undefined, 4));
+    }
 });
